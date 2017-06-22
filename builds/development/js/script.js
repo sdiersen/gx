6029ab51e537f54a97ea16f7f79e448f9d35e331
Steve Diersen <srdiersen@gmail.com>
2017-06-22 14:27:24 -0500

    gxSchedule form actually has an event listener

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -3,14 +3,15 @@
 var $ = require('jquery');
 
 var butt = document.getElementById("activetab");
+var mutt = document.getElementById("monTable");
 
-butt.addEventListener('click', function() { alert("This is annoying"); }, false);
+butt.addEventListener('click', function() { alert("This is annoying"); $('.colMon').removeClass("hidden"); }, false);
+mutt.addEventListener('click', function() { alert("Monday Heading"); $('.colMon').addClass("hidden"); }, false);
+//var mondays = document.querySelectorAll(".colMon");
 
-var mondays = document.querySelectorAll(".colMon");
-
-for (var i=0; i<mondays.length;i++) {
-    mondays[i].addEventListener('click', function() {alert("Welcome to mondays"); }, false);
-}
+//for (var i=0; i<mondays.length;i++) {
+//    mondays[i].addEventListener('click', function() {alert("Welcome to mondays"); }, false);
+//}
 var $, fill;
 
 $ = require('jquery');
